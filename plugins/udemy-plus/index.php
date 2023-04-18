71c7604f23da321e7581a013c866443216e7ea15
grum1n <grumin.el.pastas@gmail.com>
2023-04-18 21:08:02 +0300

    start  plugin

---
+++ plugins/udemy-plus/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name:       Udemy plus
+ * Plugin URI:        https://udemy.com
+ * Description:       A plugin for adding blocks to a theme.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Grumin
+ * Author URI:        https://udemy.com
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */

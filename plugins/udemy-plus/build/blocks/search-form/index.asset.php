296bd96d5d936cb1ec656499d30c5208360dc8e9
grum1n <grumin.el.pastas@gmail.com>
2023-04-19 21:14:03 +0300

    created a search form

--- plugins/udemy-plus/build/blocks/search-form/index.asset.php
+++ plugins/udemy-plus/build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '824592c2d0be4444b264');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c7786cb6ba2311a53729');

9e9dec5faf3e0a25bd24bafcad4bde3044cca89f
grum1n <grumin.el.pastas@gmail.com>
2023-04-19 18:04:26 +0300

    created search block

--- plugins/udemy-plus/build/blocks/search-form/index.asset.php
+++ plugins/udemy-plus/build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '1f16b7c31667c873ed9b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '824592c2d0be4444b264');
--- plugins/udemy-plus/src/blocks/search-form/index.js
+++ plugins/udemy-plus/src/blocks/search-form/index.js
@@ -1,10 +1,25 @@
 import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps } from '@wordpress/block-editor';
 import block from './block.json';
 import icons from '../../icons.js';
+import './main.css';
 
 registerBlockType(block.name, {
     icon: icons.primary,
     edit(){
-       return <p>search form</p> 
+        const blockProps = useBlockProps();
+
+        return (
+            // <p><!-- Search Block --></p>
+                <div {...blockProps}>
+                    <h1>Search: Your search term here</h1>
+                    <form>
+                        <input type="text" placeholder="Search" />
+                        <div className="btn-wrapper">
+                            <button type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+        )
     }
 });

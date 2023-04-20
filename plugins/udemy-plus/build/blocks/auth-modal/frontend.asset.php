45edcdded7717da41888dbb41e982b5799a0f99b
grum1n <grumin.el.pastas@gmail.com>
2023-04-20 09:47:48 +0300

    loading a View script

---
+++ plugins/udemy-plus/build/blocks/auth-modal/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'dab4853af652db9cc354');
---
+++ plugins/udemy-plus/build/blocks/auth-modal/frontend.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************************!*\
+  !*** ./src/blocks/auth-modal/frontend.js ***!
+  \*******************************************/
+console.log('Auth Modal Frontend');
+/******/ })()
+;
+//# sourceMappingURL=frontend.js.map

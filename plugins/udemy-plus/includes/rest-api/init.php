698dbc145707691770bdc370b9a7f81c018d137c
grum1n <grumin.el.pastas@gmail.com>
2023-04-21 10:02:08 +0300

    changed methods

--- plugins/udemy-plus/includes/rest-api/init.php
+++ plugins/udemy-plus/includes/rest-api/init.php
@@ -3,13 +3,13 @@
 function up_rest_api_init(){
     // example.com/wp-json/up/v1/signup
     register_rest_route('up/v1', '/signup', [
-        'methods' => 'POST',
+        'methods' => WP_REST_Server::CREATABLE,
         'callback' => 'up_rest_api_signup_handler',
         'permission_callback' => '__return_true'
     ]);
 
     register_rest_route('up/v1', '/signin', [
-        'methods' => 'POST',
+        'methods' => WP_REST_Server::EDITABLE,
         'callback' => 'up_rest_api_signin_handler',
         'permission_callback' => '__return_true'
     ]);

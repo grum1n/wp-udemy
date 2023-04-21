5e8aae6e09336cbb821e8743ce0f6fa03a09642e
grum1n <grumin.el.pastas@gmail.com>
2023-04-21 09:08:58 +0300

    signin function working fine

--- plugins/udemy-plus/includes/rest-api/signin.php
+++ plugins/udemy-plus/includes/rest-api/signin.php
@@ -12,6 +12,21 @@ function up_rest_api_signin_handler($request){
         return $response;
     }
 
+    $email = sanitize_email($params['user_login']);
+    $password = sanitize_text_field($params['password']);
+
+    //credentials
+    $user = wp_signon([
+        'user_login' => $email,
+        'user_password' => $password,
+        'remember' => true
+    ]);
+
+    // if example password is wrong
+    if(is_wp_error($user)){
+        return $response;
+    }
+
     $response['status'] = 2;
 
     return $response;

9e29b05fd57fe26299075365d638ffc1665f470f
grum1n <grumin.el.pastas@gmail.com>
2023-04-21 11:09:58 +0300

    registered recipe post function, appears on dashboard sidebar a button Books

--- plugins/udemy-plus/index.php
+++ plugins/udemy-plus/index.php
@@ -37,3 +37,4 @@ add_action('init', 'up_register_blocks');
 add_action('rest_api_init', 'up_rest_api_init');
 // WP recommends this hook for dynamic scripts
 add_action('wp_enqueue_scripts', 'up_enqueue_scripts');
+add_action('init', 'up_recipe_post_type');

98d50eff1a05bf743336b747cfc05d11fa1a087d
grum1n <grumin.el.pastas@gmail.com>
2023-04-24 21:36:53 +0300

    Updating the Permissions, tut 235 ok

--- plugins/udemy-plus/build/blocks/recipe-summary/frontend.asset.php
+++ plugins/udemy-plus/build/blocks/recipe-summary/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element'), 'version' => 'ef53c5ad9ee019c26713');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element'), 'version' => '7c1871275abc7134e011');
--- plugins/udemy-plus/src/blocks/recipe-summary/frontend.js
+++ plugins/udemy-plus/src/blocks/recipe-summary/frontend.js
@@ -1,11 +1,17 @@
 import Rating from "@mui/material/Rating/index.js";
-import { render, useState } from "@wordpress/element";//ok
+import { render, useState, useEffect } from "@wordpress/element";//ok
 //import { apiFetch } from '@wordpress/api-fetch'; // video
 import apiFetch from '@wordpress/api-fetch'; // handbook
 
 function RecipeRating(props){
     const [avgRating, setAvgRating] = useState(props.avgRating) 
     const [permission, setPermission] = useState(props.loggedIn) 
+
+    useEffect(() => {
+        if(props.ratingCount){
+            setPermission(false)
+        }
+    }, [])
 
     return (
         <Rating
@@ -41,12 +47,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const postID = parseInt(block.dataset.postId);
     const avgRating = parseFloat(block.dataset.avgRating);
     const loggedIn = !!block.dataset.loggedIn;
+    const ratingCount = !!parseInt(block.dataset.ratingCount)
 
    render(
         <RecipeRating 
             postID={postID}
             avgRating={avgRating}
             loggedIn={loggedIn}
+            ratingCount={ratingCount}
         />,
         block
    )

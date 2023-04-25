3b7414d8c7a162aecac0ebdc391293964cc8212d
grum1n <grumin.el.pastas@gmail.com>
2023-04-25 18:38:49 +0300

    Measuring Queries , tut 280 ok

--- plugins/udemy-plus/includes/blocks/daily-recipe.php
+++ plugins/udemy-plus/includes/blocks/daily-recipe.php
@@ -9,6 +9,40 @@ function up_daily_recipe_cb($atts){
         $id = up_generate_daily_recipe();
     }
 
+    /*************************
+     * start Measureing Performance 
+     * *******************/
+    $startTime = microtime(true);
+
+    global $wpdb;
+    $wpdb->get_var(
+        "SELECT ID FROM {$wpdb->posts}
+        WHERE post_status='publish' AND post_type='recipe'
+        ORDER BY rand() LIMIT 1"
+    );
+
+    $endTime = microtime(true);
+    $SQLExecutionTime = $endTime - $startTime;
+
+    $startTime = microtime(true);
+
+    new WP_Query([
+        'post_type' => 'recipe',
+        'post_status' => 'publish',
+        'posts_per_page' => 1,
+        'orderby' => 'rand'
+    ]);
+
+    $endTime = microtime(true);
+    $WPExecutionTime = $endTime - $startTime;
+
+    /***
+     * 
+     * End
+     */
+
+
+
     ob_start();
     ?>
     <div class="wp-block-udemy-plus-daily-recipe">
@@ -17,6 +51,10 @@ function up_daily_recipe_cb($atts){
             <img src="<?php echo get_the_post_thumbnail_url($id, 'full'); ?>">
             <h3><?php echo get_the_title($id); ?></h3>
         </a>
+        <p>
+            SQL Query : <?php echo  $SQLExecutionTime * 1000; ?> <br>
+            WP Query : <?php echo  $WPExecutionTime * 1000; ?> 
+        </p>
     </div>
 
     <?php

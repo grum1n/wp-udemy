740924ad7ce96beef356c058de9d506248775f8e
grum1n <grumin.el.pastas@gmail.com>
2023-04-25 10:18:43 +0300

    Editing a Social Media link, tut 261 ok

--- plugins/udemy-plus/build/blocks/team-member/index.asset.php
+++ plugins/udemy-plus/build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b438075cee8157e60e3c');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '252a90638af8e9cc8935');

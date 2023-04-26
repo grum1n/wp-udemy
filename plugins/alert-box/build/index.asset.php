41f8809aa9d80e26a9d7ef13319f913f3ac6e46a
grum1n <grumin.el.pastas@gmail.com>
2023-04-26 07:16:44 +0300

    finish alert-box

--- plugins/alert-box/build/index.asset.php
+++ plugins/alert-box/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '562711e13fd6c431fceb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ac008cf37b07542bf582');

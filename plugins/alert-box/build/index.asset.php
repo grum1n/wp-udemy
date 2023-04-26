072d15d3706b041d597b4b6d2a691cabc339b910
grum1n <grumin.el.pastas@gmail.com>
2023-04-26 06:32:27 +0300

    Block Variations ,tut 285 ok

--- plugins/alert-box/build/index.asset.php
+++ plugins/alert-box/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bcb51c0d8d2a0204104c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '53aa77a55bef5a672509');

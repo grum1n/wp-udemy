ae29907a10db49e8b69cdf04148b42aa0e1ae68f
grum1n <grumin.el.pastas@gmail.com>
2023-04-26 06:40:28 +0300

    Block styles , tut 286 ok

--- plugins/alert-box/build/index.asset.php
+++ plugins/alert-box/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '53aa77a55bef5a672509');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9feef9b55513ade64c85');
